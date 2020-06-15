63c039597d1fec8e4921ba1ea4eda29221af2b69
skylucacius <skylucacius@hotmail.com>
2020-06-15 11:38:54 -0300

    testes unitários com mocha, chai e sinon - feito

--- math.js
+++ math.js
@@ -1,7 +1,17 @@
  class Math {
-    sum = function sum(a,b) {
-        return a + b
+    sum(a,b,callback) {
+        setTimeout(() => {
+            callback(a + b)
+        }, 2500);
     };
+    multiply(a,b) {
+        return a*b
+    }
+
+    printSum(req,res,a,b) {
+        console.log(res.load('index', a+b))
+        
+    }
 
 }
 module.exports = Math;
--- test/math.spec.js
+++ test/math.spec.js
@@ -1,10 +1,36 @@
 const Math = require('../math.js')
 const assert = require('assert')
+const expect = require('chai').expect
+const sinon = require('sinon')
+const { spy } = require('sinon')
 
 describe('Classe matemática', () => {
-    it('soma dois números', () => {
+    it('soma dois números', function(done) {
         const math = new Math()
+        this.timeout(3000)
+        math.sum(5,5, (value) => {
+            expect(value).to.equal(10)
+            done()
+        })
+    })
 
-        assert.equal(math.sum(5,5), 10)
+    it('multiplica dois números', function() {
+        const math = new Math()
+        expect(math.multiply(5,5)).to.equal(25)
+    })
+    
+    it.only('chama request com os valores de soma e index', function() {
+        const req = {}
+        const res = {load: function load() {
+            console.log('Foi chamada!')
+        }}
+        const math = new Math()
+        // sinon.spy(res,'load')
+        sinon.stub(res,'load').returns('xpto')
+        math.printSum(req,res,5,5)
+        res.restore()
+        // expect(res.load.called).to.be.true
+        expect(res.load.args[0][1]).to.equal(10)
+        
     })
 })
