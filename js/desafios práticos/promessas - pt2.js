373e08a0a777dbe1672c4ee63e651187153657a5
skylucacius <skylucacius@hotmail.com>
2020-06-16 11:09:23 -0300

    desafio prático - map, filter e reduce concluído

---
+++ js/desafios práticos/promessas - pt2.js
@@ -0,0 +1,7 @@
+// const funcaoAssincrona = async (elemento) => {
+//     return new Promise((resolve,reject) => setTimeout(() => resolve(elemento + ' - promise'),2000))
+//                 .then((dado) => escreve(dado))
+// }
+
+// const items = ['a','b','c','d']
+// const itemsMapped = items.map(funcaoAssincrona)
